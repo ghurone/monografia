2ed47603238fd85e474b4fc65273924ebc1c4967
Ghuron <ghuron@usp.br>
2023-03-15 17:34:25 -0300

    Plot

--- teste_code/funcoes.py
+++ teste_code/funcoes.py
@@ -4,10 +4,11 @@ from bs4 import BeautifulSoup, SoupStrainer
 def limpa_html(text_html) -> str:
     soup = BeautifulSoup(text_html, 'lxml', parse_only=SoupStrainer('div', attrs={'data-anchor': 'Text'}))
 
-    # tags do título, citações, referências, equações e figuras foram removidas.
+    # tags do título, citações, referências, equações, tabelas e figuras foram removidas.
     remover = soup.find_all('h1') + soup.find_all('span', attrs={'class': 'ref'}) + \
            soup.find_all('div', attrs={'class':'row formula'}) + soup.find_all('div', attrs={'class': 'row fig'}) + \
-           soup.find_all('math')
+           soup.find_all('math') + soup.find_all('div', attrs={'class': 'row table'}) + soup.find_all('div', attrs={'class': 'ref-list'})
+    
     for tag in remover:
           tag.decompose()
 
